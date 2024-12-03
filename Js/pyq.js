c7aced5b41363f065be818c6b0052d8089bc9b47
Vivek Yadav <vivektalent200@gmail.com>
2024-12-03 10:26:53 +0530

    updated

--- Js/pyq.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.close-btn').forEach(button => {
-    button.addEventListener('click', function () {
-        const reveal = this.closest('.card-reveal');
-        reveal.classList.remove('active');
-    });
-});
